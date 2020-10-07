7c76429384c5ee10e237eec1dc043a642c8879b9
budiman993 <71246269+budiman993@users.noreply.github.com>
2020-10-07 10:14:12 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,68 @@
+<?php
+$_1 = 13000;
+$_2 = 15000;
+$_3 = 1000;
+$_4 = 3000;
+$_5 = 4000;
+$_6 = 5000;
+
+
+echo "<h1>Daftar Menu</h1><br>";
+echo "<table>
+<tr>
+<td><li>Ayam Goreng</td>
+<td>=</td>
+<td> Rp. $_1</td>
+</tr>
+
+<tr>
+<td><li>Ayam Bakar</td>
+<td>=</td>
+<td> Rp. $_2</td>
+</tr>
+
+<tr>
+<td><li>Ayam Sayur</td>
+<td>=</td>
+<td> Rp. $_1</td>
+</tr>
+
+<tr>
+<td><li>Tempe Goreng</td>
+<td>=</td>
+<td> Rp. $_3</td>
+</tr>
+
+<tr>
+<td><li>Tahu Goreng</td>
+<td>=</td>
+<td> Rp. $_3</td>
+</tr>
+
+<tr>
+<td><li>Soto</td>
+<td>=</td>
+<td> Rp. $_4</td>
+</tr>
+
+<tr>
+<td><li>Nasi Putih</td>
+<td>=</td>
+<td> Rp. $_6</td>
+</tr>
+
+<tr>
+<td><li>Es Teh</td>
+<td>=</td>
+<td> Rp. $_5</td>
+</tr>
+
+<tr>
+<td><li>Es Tebu</li></td>
+<td>=</td>
+<td> Rp. $_6</td>
+</tr>
+</table>"
+
+
+?>
